812cf54084ea9ec747da9fddce8ad14ab4a4857b
lauraeflor10 <65066163+lauraeflor10@users.noreply.github.com>
2022-04-23 23:22:33 +0200

    Exercise 4
    Improve code for exercise 4 - clean up

--- RR_Apr_20_21/Assignment3.py
+++ RR_Apr_20_21/Assignment3.py
@@ -85,3 +85,49 @@ df = pd.read_excel(r'C:\Users\Laura Eflor\OneDrive\Documents\GitHub\RRcourse2022
 df['State'] = df['State'].str.lower()
 
 print(df)
+
+## Exercise 4
+
+"""
+The code below is of poor quality. Improve it (you may rewrite it to R, if you find it more convenient). 
+[Data (StudentsPerfomance.csv) is available on Kaggle.](https://www.kaggle.com/spscientist/students-performance-in-exams)
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# read the file with data
+studPerformance = pd.read_csv(r'C:\Users\Laura Eflor\OneDrive\Documents\GitHub\RRcourse2022\RR_Apr_20_21\StudentsPerformance.csv')
+
+# the dataframe contains data about 5 groups
+stud_a = studPerformance.loc[studPerformance['race/ethnicity'] == 'group A']
+stud_b = studPerformance.loc[studPerformance['race/ethnicity'] == 'group B']
+stud_c = studPerformance.loc[studPerformance['race/ethnicity'] == 'group C']
+stud_d = studPerformance.loc[studPerformance['race/ethnicity'] == 'group D']
+stud_e = studPerformance.loc[studPerformance['race/ethnicity'] == 'group E']
+
+# print mean math score for each group
+groupa = stud_a['math score'].mean()
+print(groupa)
+groupb = stud_b['math score'].mean()
+print(groupb)
+groupc = stud_c['math score'].mean()
+print(groupc)
+groupd = stud_d['math score'].mean()
+print(groupd)
+groupe = stud_e['math score'].mean()
+print(groupe)
+
+# print mean math, reading, and writing scores for students who completed the test preparation course
+# and their parent obtained a degree
+degree_test = studPerformance.loc[(studPerformance['test preparation course'] == 'completed') &
+                                  (studPerformance['parental level of education'].isin(["associate's degree", "bachelor's degree", "master's degree"]))]
+print('math', degree_test['math score'].mean())
+print('reading', degree_test['reading score'].mean())
+print('writing', degree_test['writing score'].mean())
+
+# plot the histogram of math scores divided by reading scores for each student
+plt.hist(studPerformance['math score'] / studPerformance['reading score'])
+plt.title('ratio')
+plt.show()
+plt.close()
